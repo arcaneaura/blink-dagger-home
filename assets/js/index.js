b3092191c8a07a1ef4c2c2019f511a4d7755d2e1
Zhiqi Huang <Charles.hzq@gmail.com>
2017-01-29 01:55:27 -0500

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,76 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/* globals jQuery, document */
+(function($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+
+    $document.ready(function() {
+
+        var $postContent = $(".post-content");
+        $postContent.fitVids();
+
+        $(".scroll-down").arctic_scroll();
+
+        $(".menu-button, .nav-cover, .nav-close").on("click", function(e) {
+            e.preventDefault();
+            $("body").toggleClass("nav-opened nav-closed");
+        });
+
+    });
+
+    // Arctic Scroll by Paul Adam Davis
+    // https://github.com/PaulAdamDavis/Arctic-Scroll
+    $.fn.arctic_scroll = function(options) {
+
+        var defaults = {
+                elem: $(this),
+                speed: 500
+            },
+
+            allOptions = $.extend(defaults, options);
+
+        allOptions.elem.click(function(event) {
+            event.preventDefault();
+            var $this = $(this),
+                $htmlBody = $('html, body'),
+                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+                toMove;
+
+            if (offset) {
+                toMove = parseInt(offset) + 60;
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top + toMove)
+                }, allOptions.speed);
+            } else if (position) {
+                toMove = parseInt(position);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: toMove
+                }, allOptions.speed);
+            } else {
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top - 60)
+                }, allOptions.speed);
+            }
+        });
+
+    };
+})(jQuery);
+
+
+$(document).ready(function() {
+    $(document).scroll(function() {
+        $(document).scrollTop()> $(".post-header").height() ? $("#returnTop").css("bottom", "15px") : $("#returnTop").css("bottom", "-200px")
+    }), $("#returnTop").on("click", function() {
+        console.log('testuse');
+        $('html, body').animate({
+                    scrollTop: 0
+                }, 500);
+    }), console.log("Thank you! © XFeiF 🙋 2015-" + (new Date).getFullYear()),
+    console.log($('#content').scrollTop());
+});
+
